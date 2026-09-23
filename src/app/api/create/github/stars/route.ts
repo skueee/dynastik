af81fe01023549e54d97e95719e55899ffa42105
skueee <skueee@outlook.com>
2026-09-23 23:21:50 +0200

    format and console.log cleanup

--- src/app/api/create/github/stars/route.ts
+++ src/app/api/create/github/stars/route.ts
@@ -1,28 +1,26 @@
-import { generate } from "@/lib/api-handler"
+import { generate } from "@/lib/api-handler";
 import { NextRequest } from "next/server";
 
-export async function GET(
-  request: NextRequest,
-) {
+export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
-  const data = searchParams.get("data")
-  console.log(typeof data)
-  const repo = searchParams.get("repo")
-  const user = searchParams.get("user")
+  const data = searchParams.get("data");
+  const repo = searchParams.get("repo");
+  const user = searchParams.get("user");
 
-  const response = await fetch(`https://api.github.com/repos/${user}/${repo}/stargazers/count`)
-  const result = await response.json()
-  const stars: number = result.count
+  const response = await fetch(
+    `https://api.github.com/repos/${user}/${repo}/stargazers/count`,
+  );
+  const result = await response.json();
+  const stars: number = result.count;
 
-  console.log(stars)
-
-  const svg = generate(data, [{ "key": "stars", "replace": String(stars) }])
+  const svg = generate(data, [{ key: "stars", replace: String(stars) }]);
 
   return new Response(svg, {
-      status: 200,
-      headers: {
-        'Content-Type': 'image/svg+xml',
-        'Cache-Control': 'public, max-age=3600, s-maxage=3600, stale-while-revalidate=86400',
-      },
-    });
+    status: 200,
+    headers: {
+      "Content-Type": "image/svg+xml",
+      "Cache-Control":
+        "public, max-age=3600, s-maxage=3600, stale-while-revalidate=86400",
+    },
+  });
 }
--- src/lib/api-handler.ts
+++ src/lib/api-handler.ts
@@ -1,15 +1,18 @@
 // This script is meant to handle every "common" actions from the APIs (decoding, using the generator...)
 
-import createBadge from "./generate"
+import createBadge from "./generate";
 
 function base64ToUint8Array(base64: string): Uint8Array {
-  const buffer = Buffer.from(base64, 'base64')
-  return buffer
+  const buffer = Buffer.from(base64, "base64");
+  return buffer;
 }
 
 // This function takes svgData (base64 encoded), and the keys dict
-export function generate(base64SVGData: string, replaceDict: { key: string, replace: string }[]) {
-  const svgDataBytes = base64ToUint8Array(base64SVGData)
-  const svgData = new TextDecoder().decode(svgDataBytes)
-  return createBadge(svgData, replaceDict)
+export function generate(
+  base64SVGData: string,
+  replaceDict: { key: string; replace: string }[],
+) {
+  const svgDataBytes = base64ToUint8Array(base64SVGData);
+  const svgData = new TextDecoder().decode(svgDataBytes);
+  return createBadge(svgData, replaceDict);
 }
