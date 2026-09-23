936cb38a66520624c98814874a22356fdb3004e9
skueee <skueee@outlook.com>
2026-09-23 22:35:41 +0200

    Added (buggy) stars route

--- src/lib/api-handler.ts
+++ src/lib/api-handler.ts
@@ -2,8 +2,14 @@
 
 import createBadge from "./generate"
 
+function base64ToUint8Array(base64: string): Uint8Array {
+  const binaryString = atob(base64);
+  return Uint8Array.from(binaryString, (char) => char.charCodeAt(0));
+}
+
 // This function takes svgData (base64 encoded), and the keys dict
-export function generate(base64SVGData: string, replaceDict: { key: string; replace: string }[]) {
-  const svgData = atob(base64SVGData)
+export function generate(base64SVGData: string, replaceDict: { key: string, replace: string }[]) {
+  const svgDataBytes = base64ToUint8Array(base64SVGData)
+  const svgData = new TextDecoder().decode(svgDataBytes)
   return createBadge(svgData, replaceDict)
 }
